ea62aa37d1cbc9abffb7f022d30dd6d2338006ac
TheEgid <egid.f1@gmail.com>
2021-10-26 15:54:06 +0300

    Total rebuild

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
-import express from 'express';
-// rest of the code remains same
-const app = express();
-const PORT = 3000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+import dotenv from 'dotenv';
+import log4js from 'log4js';
+import { UserController, SimpleController } from './controller/user-controller';
+import express, { Express, RequestHandler } from 'express';
+import bodyParser from 'body-parser';
+import httpContext from 'express-http-context';
+import { useExpressServer } from 'routing-controllers';
+import { GlobalErrorHandler } from './middleware/global-error-handler';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../src/swagger/openapi.json';
+import cors from 'cors';
+import config from 'config';
+
+dotenv.config();
+const logger = log4js.getLogger();
+logger.level = process.env.LOG_LEVEL;
+
+// const port = process.env.PORT;
+const port = config.get('PORT');
+
+const app: Express = express();
+app.use(cors() as RequestHandler);
+app.use(bodyParser.json());
+app.use(httpContext.middleware);
+useExpressServer(app, {
+  controllers: [UserController, SimpleController], // we specify controllers we want to use
+  middlewares: [GlobalErrorHandler],
+  defaultErrorHandler: false
 });
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.use((req, res, next) => {
+//   httpContext.ns.bindEmitter(req);
+//   httpContext.ns.bindEmitter(res);
+//   next();
+// });
+
+app.listen(port, () => console.log(`⚡️[server]: Server is running at http://localhost:${port}`));
