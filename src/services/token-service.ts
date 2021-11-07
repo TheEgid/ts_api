7f5a34e07419f3406f5f0316108734e284e3bfa8
TheEgid <egid.f1@gmail.com>
2021-11-07 20:33:50 +0300

    Fix getUserByToken

--- src/services/token-service.ts
+++ src/services/token-service.ts
@@ -1,17 +1,15 @@
 import { getConnection } from "typeorm";
 import User from "../models/user-entity";
-import Token from "../models/token-entity";
 import TokenRepository from "../repos/tokens-repository";
 
 export default class TokenService {
-  static async getUserByToken(refreshToken: string): Promise<User> | undefined {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-return
+  static async getUserByToken(accessToken: string): Promise<User> | undefined {
     return await getConnection(process.env.DB_NAME)
-      .getRepository(Token)
-      .query(
-        `SELECT * FROM "user", "token" where "token".id = "user"."tokenId" and "token"."refreshToken" = $1`,
-        [refreshToken]
-      );
+      .getRepository(User)
+      .createQueryBuilder("user")
+      .leftJoinAndSelect("token", "token", "token.id = user.tokenId")
+      .where("token.accessToken = :1", { 1: accessToken })
+      .getOne();
   }
 
   static async setToken(user: User): Promise<User> {
